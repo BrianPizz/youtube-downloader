934af499dd49be30c2241ea98ca519649c7ad351
BrianPizz <bpizzimentijr@outlook.com>
2024-04-04 14:28:08 -0700

    download highest res

--- youtube.py
+++ youtube.py
@@ -7,7 +7,7 @@ def download_video(url, save_path):
         yt = YouTube(url)
         streams = yt.streams.filter(progressive=True, file_extension="mp4")
         highest_res_stream = streams.get_highest_resolution()
+        highest_res_stream.download(output_path=save_path)
         
-
     except Exception as e:
         print(e)
