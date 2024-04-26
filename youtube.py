32b3aff5631b71cc259e1cc2522fb96fe34c117f
BrianPizz <bpizzimentijr@outlook.com>
2024-04-25 21:59:21 -0700

    reformat conditional

--- youtube.py
+++ youtube.py
@@ -30,7 +30,7 @@ if __name__ == "__main__":
     video_url = input("Please enter a YouTube url: ")
     save_dir = open_file_dialog()
 
-    if not save_dir:
-        print("Invalid save location.")
-    else:
+    if save_dir:
         download_video(video_url, save_dir)
+    else:
+        print("Invalid save location.")
